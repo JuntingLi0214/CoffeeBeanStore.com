e2e834b173cb05278f30c672b7eaa49f7072bc2e
JuntingLi0214 <jlii0214@student.monash.edu>
2024-05-29 08:43:47 +1000

    Delete 30672708_Junting Li_FIT1050 Assignment 3 directory

--- 30672708_Junting Li_FIT1050 Assignment 3/layout/script/faq.js
+++
@@ -1,7 +0,0 @@
-const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
-for (const item of faqItems) {
-    item.classList.remove('active');
-    item.addEventListener('click', function() {
-        this.classList.toggle('active');
-    });
-}
